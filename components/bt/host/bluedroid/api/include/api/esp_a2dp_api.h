088d3436b27d317fff41a1142bca3aa6f69c791a
Chris Johnson <tnifc@protonmail.com>
2022-02-23 10:29:10 -0500

    component/bt: Define aptx-ll codec info storage

--- components/bt/host/bluedroid/api/include/api/esp_a2dp_api.h
+++ components/bt/host/bluedroid/api/include/api/esp_a2dp_api.h
@@ -41,6 +41,7 @@ typedef struct {
 #define ESP_A2D_CIE_LEN_ATRAC        (7)
 #define ESP_A2D_CIE_LEN_APTX         (7)
 #define ESP_A2D_CIE_LEN_APTX_HD      (11)
+#define ESP_A2D_CIE_LEN_APTX_LL      (7)
     union {
         uint8_t sbc[ESP_A2D_CIE_LEN_SBC];      /*!< SBC codec capabilities */
         uint8_t m12[ESP_A2D_CIE_LEN_M12];      /*!< MPEG-1,2 audio codec capabilities */
@@ -48,6 +49,7 @@ typedef struct {
         uint8_t atrac[ESP_A2D_CIE_LEN_ATRAC];  /*!< ATRAC family codec capabilities */
         uint8_t aptx[ESP_A2D_CIE_LEN_APTX];    /*!< APTX codec capabilities */
         uint8_t aptx_hd[ESP_A2D_CIE_LEN_APTX_HD];    /*!< APTX-HD codec capabilities */
+        uint8_t aptx_ll[ESP_A2D_CIE_LEN_APTX_LL];    /*!< APTX-LL codec capabilities */
     } cie;                                     /*!< A2DP codec information element */
 } __attribute__((packed)) esp_a2d_mcc_t;
 
