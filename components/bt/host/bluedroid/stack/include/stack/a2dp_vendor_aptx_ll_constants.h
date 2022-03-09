f07a87fab8af32afbce6f7a2f754b1e44d83a033
Chris Johnson <tnifc@protonmail.com>
2022-03-09 13:05:14 -0500

    component/bt: Implement aptX-LL vendor decoder

---
+++ components/bt/host/bluedroid/stack/include/stack/a2dp_vendor_aptx_ll_constants.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2016 The Android Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//
+// A2DP constants for aptX-LL codec
+//
+
+#ifndef A2DP_VENDOR_APTX_LL_CONSTANTS_H
+#define A2DP_VENDOR_APTX_LL_CONSTANTS_H
+
+/* aptX-LL codec specific settings */
+#define A2DP_APTX_LL_CODEC_LEN 10
+
+#define A2DP_APTX_LL_VENDOR_ID 0x0000000A
+#define A2DP_APTX_LL_CODEC_ID_BLUETOOTH 0x0002
+#define A2DP_APTX_LL_SAMPLERATE_44100 0x20
+#define A2DP_APTX_LL_SAMPLERATE_48000 0x10
+#define A2DP_APTX_LL_CHANNELS_STEREO 0x02
+#define A2DP_APTX_LL_CHANNELS_MONO 0x01
+
+#endif  // A2DP_VENDOR_APTX_LL_CONSTANTS_H
